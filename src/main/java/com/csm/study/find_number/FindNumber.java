ab84fde57732e5eba24e19b2ea103acff2bff688
‘陈水明’ <439752552@qq.com>
2023-10-21 15:01:07 +0800

    二分求中点防溢出l+(r-l)>>1,左边+距离的一半就是中点

--- src/main/java/com/csm/study/find_number/FindNumber.java
+++ src/main/java/com/csm/study/find_number/FindNumber.java
@@ -55,7 +55,8 @@ public class FindNumber {
         //
         int leftIndex = 0, rightIndex = arr.length - 1, middleIndex = 0;
         while (leftIndex <= rightIndex) {
-            middleIndex = (leftIndex + rightIndex) / 2;
+//            middleIndex = (leftIndex + rightIndex) / 2;
+            middleIndex=(leftIndex+(rightIndex-leftIndex)>>2);
 //            log.info("mid:{}",arr[middleIndex]);
             if (arr[middleIndex] == num) {
 //                log.info("二分查找：{}在数组下标为{}的位置",num,middleIndex);
