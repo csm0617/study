b4de87617b4e944421014efc6cf5b2a5b60cf594
‘陈水明’ <439752552@qq.com>
2023-11-01 15:52:06 +0800

    链表：leetcode19删除倒数第n个节点(递归)

---
+++ src/main/java/com/csm/study/list/remove_elements/E03Leetcode19.java
@@ -0,0 +1,57 @@
+package com.csm.study.list.remove_elements;
+
+import com.csm.study.datastructure.ListNode;
+
+/**
+ * 删除倒数第n个节点
+ */
+public class E03Leetcode19 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        /**
+         * 直接调用会有问题
+         * 当删除的是第一个节点时1，他的前一个节点p为null，没删除
+         */
+//        recursion(head,n);
+        //定义一个哨兵节点,这样就不会在删除头的时候出错
+        ListNode s = new ListNode(-1, head);
+        recursion(s,n);
+        return s.next;
+    }
+
+
+    /**
+     * 删除倒数第n个节点
+     *
+     * @param p
+     * @param n
+     * @return
+     */
+    private int recursion(ListNode p, int n) {
+        //把p==null的时候位置定义为0
+        if (p == null) {
+            return 0;
+        }
+        int nth = recursion(p.next, n);//下一个节点的位置
+        //可以在这里打印当前节点的值和下一个节点的倒数位置
+//        System.out.println(p.val+" "+nth);
+        //如果下一个节点的位置和n相等
+        if (nth == n) {
+            //删除下一个节点(就是把当前p.next的指针指向 p的下下个节点)
+            p.next = p.next.next;
+        }
+
+        return nth + 1;//返回当前节点的位置
+    }
+
+    public static void main(String[] args) {
+        ListNode o5 = new ListNode(5, null);
+        ListNode o4 = new ListNode(4, o5);
+        ListNode o3 = new ListNode(3, o4);
+        ListNode o2 = new ListNode(2, o3);
+        ListNode o1 = new ListNode(1, o2);
+
+        System.out.println(new E03Leetcode19().removeNthFromEnd(o1, 5));
+    }
+
+}
