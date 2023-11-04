e1269b1be72f0d4b55ccbdd63d9d4d1b2145ddc0
‘陈水明’ <439752552@qq.com>
2023-11-04 11:07:05 +0800

    队列：接口方法的定义

---
+++ src/main/java/com/csm/study/datastructure/queue/structure/Queue.java
@@ -0,0 +1,30 @@
+package com.csm.study.datastructure.queue.structure;
+
+public interface Queue<E> {
+
+    /**
+     * 向队列尾插入值
+     * @param value 待插入值
+     * @return 插入成功返回true，插入失败返回false
+     */
+    boolean offer(E value);
+
+    /**
+     * 从队列头获取值，并移除
+     * @return 如果队列非空返回队头值，否则返回null
+     */
+    E poll();
+
+    /**
+     * 从队列头获取值，不移除
+     * @return 如果队列非空返回队头值，否则返回null
+     */
+    E peek();
+
+    /**
+     * 检查队列是否为空
+     * @return 空返回ture,否则返回false
+     */
+    boolean isEmpty();
+
+}
