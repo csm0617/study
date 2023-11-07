ba98ea46623c867a23543a2f2eed8bb7519747d7
‘陈水明’ <439752552@qq.com>
2023-11-07 16:56:29 +0800

    栈：接口方法定义

---
+++ src/main/java/com/csm/study/datastructure/stack/Stack.java
@@ -0,0 +1,34 @@
+package com.csm.study.datastructure.stack;
+
+public interface Stack<E>{
+    /**
+     * 向栈顶压入元素
+     * @param value 待压入值
+     * @return 压入成功返回true，否则返回false
+     */
+    boolean push(E value);
+
+    /**
+     * 从栈顶弹出元素
+     * @return 栈非空返回栈顶元素，栈为空返回null
+     */
+    E pop();
+
+    /**
+     * 返回栈顶元素，不弹出
+     * @return 栈非空返回栈顶元素，栈为空返回null
+     */
+    E peek();
+
+    /**
+     * 判断栈是否为空
+     * @return 空返回true,否则返回false
+     */
+    boolean isEmpty();
+
+    /**
+     * 判断栈是否已满
+     * @return 满返回true,否则返回false
+     */
+    boolean isFull();
+}
