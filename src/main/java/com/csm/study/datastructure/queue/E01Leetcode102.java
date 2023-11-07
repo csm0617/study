bf9fea2a63c97a72f1068f6c15df08f61486450e
‘陈水明’ <439752552@qq.com>
2023-11-07 14:06:32 +0800

    队列：leetcode102二叉树的层序遍历(以及按层级打印)

--- src/main/java/com/csm/study/datastructure/queue/E01Leetcode102.java
+++ src/main/java/com/csm/study/datastructure/queue/E01Leetcode102.java
@@ -2,6 +2,10 @@ package com.csm.study.datastructure.queue;
 
 import com.csm.study.datastructure.queue.structure.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 二叉树的层序遍历（也不一定非要是二叉的）
  */
@@ -32,17 +36,65 @@ public class E01Leetcode102 {
         //先将根节点加入到队列中
         queue.offer(root);      //[ 1 ]
         //遍历队列，直到队列为空
+        int c1 = 1;//当前层节点数
         while (!queue.isEmpty()) {
-            //队头元素出队
-            TreeNode n = queue.poll();
-            System.out.println(n);//TreeNode重写了toString
-            //判断  队头的左右孩子是不是为空，不为空则依次将左右孩子加入队列
-            if (n.left != null) {
-                queue.offer(n.left);  //[ 2 ]
+            int c2 = 0;//下一层节点数
+            for (int i = 0; i < c1; i++) {
+                //队头元素出队
+                TreeNode n = queue.poll();
+                System.out.print(n + "\t");//TreeNode重写了toString
+                //判断  队头的左右孩子是不是为空，不为空则依次将左右孩子加入队列
+                if (n.left != null) {
+                    queue.offer(n.left);  //[ 2 ]
+                    c2++;
+                }
+                if (n.right != null) {
+                    queue.offer(n.right);// [ 2 3 ]
+                    c2++;
+                }
             }
-            if (n.right != null) {
-                queue.offer(n.right);// [ 2 3 ]
-            }
+            //内层循环结束
+            c1 = c2;
+            System.out.println();
         }
+    }
+
+
+    //LeetCode102
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        //传过来的是空节点
+        if (root == null) {
+            return result;
+        }
+        //构造一个存放节点的链式队列
+//        LinkedListQueue<TreeNode> queue = new LinkedListQueue<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        //先将根节点加入到队列中
+        queue.offer(root);      //[ 1 ]
+        //遍历队列，直到队列为空
+        int c1 = 1;//当前层节点数
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int c2 = 0;//下一层节点数
+            for (int i = 0; i < c1; i++) {
+                //队头元素出队
+                TreeNode n = queue.poll();
+                level.add(n.val);
+                //判断  队头的左右孩子是不是为空，不为空则依次将左右孩子加入队列
+                if (n.left != null) {
+                    queue.offer(n.left);  //[ 2 ]
+                    c2++;
+                }
+                if (n.right != null) {
+                    queue.offer(n.right);// [ 2 3 ]
+                    c2++;
+                }
+            }
+            //内层循环结束
+            result.add(level);
+            c1 = c2;
+        }
+        return result;
     }
 }
