5d44c6180b0451ed20f5e6e60e329c748c69e0e2
‘陈水明’ <439752552@qq.com>
2023-11-16 14:57:22 +0800

    二叉搜索树：方法和定义，以及获取关键字的对应值

---
+++ src/main/java/com/csm/study/datastructure/bianry_search_tree/structure/BSTTree1.java
@@ -0,0 +1,129 @@
+package com.csm.study.datastructure.bianry_search_tree.structure;
+
+public class BSTTree1 {
+    public BSTNode root;//根节点
+
+    public static class BSTNode {
+        int key;
+        Object value;
+        BSTNode left;
+        BSTNode right;
+
+        public BSTNode(int key) {
+            this.key = key;
+        }
+
+        public BSTNode(int key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public BSTNode(int key, Object value, BSTNode left, BSTNode right) {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 根据关键字查找对应值
+     *
+     * @param key 关键字
+     * @return 关键字对应的值
+     */
+    public Object get(int key) {
+        return doGet(root, key);
+    }
+
+    private Object doGet(BSTNode node, int key) {
+        //结束递归
+        if (node == null) {
+            return null;
+        }
+        if (key < node.key) {//往左找
+            return doGet(node.left, key);
+        } else if (node.key < key) {//往右找
+            return doGet(node.right, key);
+        } else {//找到了
+            return node.value;
+        }
+    }
+    /**
+     * 根据关键字查找对应值（非递归）
+     *
+     * @param key 关键字
+     * @return 关键字对应的值
+     */
+    public Object get1(int key) {
+        BSTNode node = root;
+        while (node != null) {
+            if (node.key < key) {
+                node = node.right;
+            } else if (node.key > key) {
+                node = node.left;
+            } else {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 查找最小关键字对应值
+     *
+     * @return 关键字对应的值
+     */
+    public Object min() {
+        return null;
+    }
+
+    /**
+     * 查找最大关键字对应值
+     *
+     * @return 关键字对应的值
+     */
+    public Object max() {
+        return null;
+    }
+
+    /**
+     * 存储关键字和对应值
+     *
+     * @param key   关键字
+     * @param value 值
+     */
+    public void put(int key, Object value) {
+
+    }
+
+    /**
+     * 查找关键字的前驱值
+     *
+     * @param key 关键字
+     * @return 前驱值
+     */
+    public Object successor(int key) {
+        return null;
+    }
+
+    /**
+     * 查找关键字的后继值
+     *
+     * @param key 关键字
+     * @return 后继值
+     */
+    public Object predecessor(int key) {
+        return null;
+    }
+
+    /**
+     * 根据关键字删除
+     *
+     * @param key 关键字
+     * @return 被删除关键字对应值
+     */
+    public Object delete(int key) {
+        return null;
+    }
+}
---
+++ src/test/java/com/csm/study/binary_search_tree/TestBSTTree1.java
@@ -0,0 +1,55 @@
+package com.csm.study.binary_search_tree;
+
+import com.csm.study.datastructure.bianry_search_tree.structure.BSTTree1;
+import org.junit.jupiter.api.Test;
+
+public class TestBSTTree1 {
+    public BSTTree1 create() {
+        /*
+                                4
+                              /   \
+                             2     6
+                            / \   / \
+                           1   3 5   7
+         */
+
+        BSTTree1.BSTNode n1 = new BSTTree1.BSTNode(1, "张无忌");
+        BSTTree1.BSTNode n3 = new BSTTree1.BSTNode(3, "宋青书");
+        BSTTree1.BSTNode n2 = new BSTTree1.BSTNode(2, "周芷若", n1, n3);
+
+        BSTTree1.BSTNode n5 = new BSTTree1.BSTNode(5, "说不得");
+        BSTTree1.BSTNode n7 = new BSTTree1.BSTNode(7, "殷离");
+        BSTTree1.BSTNode n6 = new BSTTree1.BSTNode(6, "赵敏", n5, n7);
+        BSTTree1.BSTNode root = new BSTTree1.BSTNode(4, "小昭", n2, n6);
+        BSTTree1 tree = new BSTTree1();
+        tree.root = root;
+        return tree;
+
+    }
+    @Test
+    public void get(){
+        BSTTree1 tree = create();
+        System.out.println(tree.get(1));
+        System.out.println(tree.get(2));
+        System.out.println(tree.get(3));
+        System.out.println(tree.get(4));
+        System.out.println(tree.get(5));
+        System.out.println(tree.get(6));
+        System.out.println(tree.get(7));
+        System.out.println(tree.get(8));
+        System.out.println(tree.get(9));
+    }
+    @Test
+    public void get1(){
+        BSTTree1 tree = create();
+        System.out.println(tree.get1(1));
+        System.out.println(tree.get1(2));
+        System.out.println(tree.get1(3));
+        System.out.println(tree.get1(4));
+        System.out.println(tree.get1(5));
+        System.out.println(tree.get1(6));
+        System.out.println(tree.get1(7));
+        System.out.println(tree.get1(8));
+        System.out.println(tree.get1(9));
+    }
+}
